0e22e8607619be80257e0a9a97150e7c1c97d680
Nathan <nathancanwood@gmail.com>
2023-01-23 13:34:35 +0000

    working framework

--- app/src/index.js
+++ app/src/index.js
@@ -1,29 +1,42 @@
+import "./sass/style.scss";
 import * as THREE from 'three';
 // import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 // import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
-import "./sass/style.scss";
 
-const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+class BBB {
+    constructor() {
+        this.scene = new THREE.Scene();
+        this.camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+        
+        this.renderer = new THREE.WebGLRenderer();
+        document.body.appendChild( this.renderer.domElement );
+        this.windowResize();
+        window.addEventListener('resize', () => { this.windowResize() })
+        
+        const geometry = new THREE.BoxGeometry( 1, 1, 1 );
+        const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+        this.cube = new THREE.Mesh( geometry, material );
+        this.scene.add( this.cube );
+        
+        this.camera.position.z = 5;
 
-const renderer = new THREE.WebGLRenderer();
-renderer.setSize( window.innerWidth, window.innerHeight );
-document.body.appendChild( renderer.domElement );
+        this.loop();
+    }
+    
+    windowResize() {
+        this.camera.aspect = window.innerWidth / window.innerHeight;
+        this.camera.updateProjectionMatrix();
+        this.renderer.setSize( window.innerWidth, window.innerHeight );
+    }
 
-const geometry = new THREE.BoxGeometry( 1, 1, 1 );
-const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-const cube = new THREE.Mesh( geometry, material );
-scene.add( cube );
+    loop() {
+        requestAnimationFrame(() => { this.loop(); });
 
-camera.position.z = 5;
+        this.cube.rotation.x += 0.01;
+        this.cube.rotation.y += 0.01;
 
-function animate() {
-    requestAnimationFrame( animate );
+        this.renderer.render( this.scene, this.camera );
+    }
+}
 
-    cube.rotation.x += 0.01;
-    cube.rotation.y += 0.01;
-
-    renderer.render( scene, camera );
-};
-
-animate();
+window.bbb = new BBB();
