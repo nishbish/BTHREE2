686c8d817a3c29bbeb60e439a5719ecb41a46342
Nathan <nathancanwood@gmail.com>
2023-01-23 21:22:11 +0000

    organised added pixels

--- app/src/index.js
+++ app/src/index.js
@@ -1,41 +1,68 @@
 import "./sass/style.scss";
 import * as THREE from 'three';
-// import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 // import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
+import { EffectComposer } from 'three/addons/postprocessing/EffectComposer.js';
+import { RenderPixelatedPass } from 'three/addons/postprocessing/RenderPixelatedPass.js';
 
 class BBB {
     constructor() {
         this.scene = new THREE.Scene();
+        this.init();
+
+        this.makeCube();
+    }
+
+    init() {
+        this.initCamera();
+        this.initRenderer();
+        this.initControls();
+    }
+
+    initCamera() {
         this.camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
-        
+        this.camera.position.z = 5;
+    }
+
+    initRenderer() {
+        //renderer
         this.renderer = new THREE.WebGLRenderer();
-        document.body.appendChild( this.renderer.domElement );
         this.windowResize();
         window.addEventListener('resize', () => { this.windowResize() })
-        
-        const geometry = new THREE.BoxGeometry( 1, 1, 1 );
-        const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-        this.cube = new THREE.Mesh( geometry, material );
-        this.scene.add( this.cube );
-        
-        this.camera.position.z = 5;
+        document.body.appendChild( this.renderer.domElement );
 
-        this.loop();
+        //composer effects
+        this.effects = {
+            composer: new EffectComposer( this.renderer ),
+            renderPixelatedPass: new RenderPixelatedPass( 6, this.scene, this.camera )
+        }
+        
+        this.effects.composer.addPass(this.effects.renderPixelatedPass);
+        this.animate();
     }
-    
+
+    initControls() {
+        this.orbitControlscontrols = new OrbitControls(this.camera, this.renderer.domElement);
+    }
+
+    animate() {
+        requestAnimationFrame(() => { this.animate(); });
+        
+        //this.renderer.render( this.scene, this.camera );
+        this.effects.composer.render();
+    }
+
     windowResize() {
         this.camera.aspect = window.innerWidth / window.innerHeight;
         this.camera.updateProjectionMatrix();
         this.renderer.setSize( window.innerWidth, window.innerHeight );
     }
 
-    loop() {
-        requestAnimationFrame(() => { this.loop(); });
-
-        this.cube.rotation.x += 0.01;
-        this.cube.rotation.y += 0.01;
-
-        this.renderer.render( this.scene, this.camera );
+    makeCube() {
+        const geometry = new THREE.BoxGeometry( 1, 1, 1 );
+        const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+        this.cube = new THREE.Mesh( geometry, material );
+        this.scene.add( this.cube );
     }
 }
 
